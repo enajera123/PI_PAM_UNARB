17c421c24ef2aae8c89bd358fc4f51798a69d9d3
enajera <estebannajera42@gmail.com>
2024-04-26 21:23:23 -0600

    feat: get endpoint in users

---
+++ src/app/api/users/[id]/route.ts
@@ -0,0 +1,11 @@
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+// export async function PUT(req: NextRequest, res: NextResponse) {
+//     try {
+        
+//         return NextResponse.json(users, { status: 200 });
+//     } catch (error) {
+//         return NextResponse.json(error, { status: 500 });
+//     }
+// }
---
+++ src/app/api/users/route.ts
@@ -0,0 +1,11 @@
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest, res: NextResponse) {
+    try {
+        const users = await prisma.users.findMany({});
+        return NextResponse.json(users, { status: 200 });
+    } catch (error) {
+        return NextResponse.json(error, { status: 500 });
+    }
+}
