a315a4a2de00f94329c0ca0a4e817ad509f98643
DARVAR2K <darvar2k@gmail.com>
2024-04-30 23:37:10 -0600

    feat: participantOnCourse crud

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,22 +1,30 @@
 export type ParameterId = {
-    params: { id: string };
-  };
-  
-  export type ParameterFirstName = {
-    params: { firstName: string };
-  };
-  
-  export type ParameterCourseNumber = {
-    params: { courseNumber: string };
-  };
-  
-  export type ParameterName = {
-    params: { name: string };
-  };
-  
-  export type ParameterIdentification = {
-    params: { identification: string };
-  };
-  export type ParameterEmail = {
-    params: { email: string };
-  };
+  params: { id: string };
+};
+
+export type ParameterParticipantId = {
+  params: { participantId: string };
+};
+
+export type ParameterCourseId = {
+  params: { courseId: string };
+};
+
+export type ParameterFirstName = {
+  params: { firstName: string };
+};
+
+export type ParameterCourseNumber = {
+  params: { courseNumber: string };
+};
+
+export type ParameterName = {
+  params: { name: string };
+};
+
+export type ParameterIdentification = {
+  params: { identification: string };
+};
+export type ParameterEmail = {
+  params: { email: string };
+};
