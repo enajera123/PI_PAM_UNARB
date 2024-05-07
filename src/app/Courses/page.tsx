33bc7ec5929e02360ec09b2fb1ae5a8f8020f1d7
KendallSN <kendallsalazarnavarro@gmail.com>
2024-05-07 01:20:07 -0600

    feat(screens creation):The information,record and report screens were created

--- src/app/Courses/page.tsx
+++ src/app/Courses/page.tsx
@@ -1,3 +1,26 @@
+import Select from "@/components/Select/Select";
+import {FaUsers} from "react-icons/fa";
 export default function CoursesPage(){
-    return <p>Cursos</p>
+    const options = [
+        { value: "US", label: "United States" },
+        { value: "CA", label: "Canada" },
+        { value: "FR", label: "France" },
+        { value: "DE", label: "Germany" },
+      ];
+    return (
+        <div className="">
+            <Select
+            label="Rol"
+            placeholder="Escoja un rol"
+            icon={<FaUsers color="white" />}
+            options={options}
+            />
+            <Select
+            label="Rol"
+            placeholder="Escoja un rol"
+            icon={<FaUsers color="white" />}
+            options={options}
+            />
+        </div>
+    )
 }
