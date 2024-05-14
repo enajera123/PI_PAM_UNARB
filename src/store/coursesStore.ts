c9575c030a9a64d1ad7673a3f5f0622d42f08adf
Heilen99 <heilen.segura.cordero@est.una.ac.cr>
2024-05-14 17:22:58 -0600

    Implementation of the remaining stores

---
+++ src/store/coursesStore.ts
@@ -0,0 +1,66 @@
+import { create } from "zustand";
+import { Course } from "@/types/types";
+import { CoursesState } from "@/types/state";
+import {
+    getCourses,
+    getCourseById,
+    createCourse,
+    updateCourse,
+    deleteCourse,
+    getCourseByCourseNumber,
+    getCourseByName,
+} from "@/services/coursesService";
+
+export const useCourseStore = create<CoursesState>((set) => ({
+    courses: [] as Course[],
+    setCourses: (courses) => set({ courses }),
+
+    getCourses: async () => {
+        const courses = await getCourses();
+        set({ courses });
+    },
+
+    getCourseById: async (id: number) => {
+        const course = await getCourseById(id);
+        set((state) => ({
+            courses: state.courses.map((c) => (c.id === id ? course : c)),
+        }));
+    },
+
+    postCourse: async (course: Course) => {
+        const newCourse = await createCourse(course);
+        if (newCourse) {
+            set((state) => ({ courses: [...state.courses, newCourse] }));
+        }
+    },
+
+    putCourse: async (id: number, course: Course) => {
+        const updatedCourse = await updateCourse(id, course);
+        set((state) => ({
+            courses: state.courses.map((c) => (c.id === id ? updatedCourse : c)),
+        }));
+    },
+
+    deleteCourse: async (id: number) => {
+        await deleteCourse(id);
+        set((state) => ({
+            courses: state.courses.filter((c) => c.id !== id),
+        }));
+    },
+
+    getCourseByCourseNumber: async (courseNumber: string) => {
+        const course = await getCourseByCourseNumber(courseNumber);
+        set((state) => ({
+            courses: state.courses.map((c) =>
+                c.courseNumber === courseNumber ? course : c
+            ),
+        }));
+    },
+
+    getCourseByName: async (name: string) => {
+        const course = await getCourseByName(name);
+        set((state) => ({
+            courses: state.courses.map((c) => (c.name === name ? course : c)),
+        }));
+    },
+}));
